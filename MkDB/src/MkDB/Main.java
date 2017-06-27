f1c3f4789985b2550eb166ffda878ead2d174506
chomk <chomgopq263@gmail.com>
2017-06-27 09:52:54 +0900

    Search & Insert & Delete

--- MkDB/src/MkDB/Main.java
+++ MkDB/src/MkDB/Main.java
@@ -2,34 +2,52 @@ package MkDB;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String[] args) {
-		try {
-			BufferedReader reader = new BufferedReader(new FileReader("input.txt"));
-			BufferedWriter writer = new BufferedWriter(new FileWriter("output.txt", true));
-			
-			// 입력 받아서 해당 operation 수행하기 (while, switch)
-			String line = null;
-			while ((line = reader.readLine()) != null) {
-				System.out.println(line);
-				writer.write(line);
-				writer.newLine();
-			}
-			
-//			reader.close();
-			writer.close();
-			
-		} catch (FileNotFoundException e) {
-			System.out.println("FileNotFoundException");
-		} catch (Exception e) {
-			System.out.println("Exception");			
+	public static void main(String[] args) throws IOException {
+		Operation op = new Operation();
+		
+		File data = new File("data.txt");
+		BufferedReader reader = new BufferedReader(new FileReader(data));
+		String line = null;
+		
+		//Open & Load DB File
+		int lineCount = 0;
+		while ((line = reader.readLine()) != null) {
+			lineCount++;
+		}
+		op.data = new int[lineCount];
+		
+		reader = new BufferedReader(new FileReader(data));
+		line = null;
+		for(int i = 0; i < lineCount; i++) {
+			op.data[i] = Integer.parseInt(reader.readLine());
 		}
 		
+		reader.close();
+		
+		Scanner scanner = new Scanner(System.in);
+		while (true) {
+			String input = scanner.nextLine();
+			String[] inputs = input.split("[.]");
+			
+			if (inputs[0] == "insert") {
+				op.insert(data, Integer.parseInt(inputs[1]));
+			} else if (inputs[0] == "delete") {
+				op.delete(data, Integer.parseInt(inputs[1]));
+			} else if (inputs[0] == "search") {
+				System.out.println(op.search(Integer.parseInt(inputs[1])));
+			} else { //sort
+				op.sort();
+			}
+		}
 	}
 
 }
--- MkDB/src/MkDB/Operation.java
+++ MkDB/src/MkDB/Operation.java
@@ -1,27 +1,60 @@
 package MkDB;
 
 import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class Operation {
-	
-//	public static int search(int key) {
-//		
-//	}
-	
-	public static void insert(BufferedWriter writer, int key) {
+	BufferedWriter writer = null;
+	int[] data;
+
+	//DB에 key 삽입 & 저장
+	public void insert(File file, int key) {
 		try {
+			writer = new BufferedWriter(new FileWriter(file, true)); //파일 append
 			writer.write(key);
+			writer.close();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
 	
-//	public static String delete(int key) {
-//		
-//	}
+	/*
+	 * key 삭제 후 파일 DB에 저장
+	 * 탐색 --> 그 위치 무시하고 저장
+	 */
+	public void delete(File file, int key) {
+		try {
+			writer = new BufferedWriter(new FileWriter(file, false)); //파일 덮어쓰기
+			int position = search(key);
+			
+			for (int i = 0; i < position; i++) {
+				writer.write(data[i] + '\n');
+			}
+			
+			for (int i = position + 1; i < data.length; i++) {
+				writer.write(data[i] + '\n');
+			}
+			
+			writer.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 	
-	public static void sort() {
+	//key에 해당하는 index 리턴
+	public int search(int key) {
+		for (int i = 0; i < data.length; i++) {
+			if (key == data[i]) {
+				return i;
+			}
+		}
+		return -1;
+	}
+
+	//Sort data array (not data file!)
+	public void sort() {
 		
 	}
 }
